d0a9f657cdf7325862697541c223f7287eaea6dc
sri1701 <sri2142@gmail.com>
2021-07-12 22:58:29 -0400

    Update ILoginService.cs

--- Services/ILoginService.cs
+++ Services/ILoginService.cs
@@ -1,4 +1,4 @@
-﻿using LoginMVC.Models;
+using LoginMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +8,16 @@ namespace LoginMVC.Services
 {
     public interface ILoginService
     {
+        /// <summary>
+        /// Logs In the user and saves to Database
+        /// </summary>
+        /// <param name="model"></param>
         public void Login(LoginModel model);
 
+        /// <summary>
+        /// Get the list of Logged in users
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<LoginModel> GetUsers();
     }
 }
